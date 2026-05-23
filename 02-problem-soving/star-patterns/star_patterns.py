47d16e30a6a9a8bd0dd55f1ec9d74f953f80ffda
B <65490597+baus5@users.noreply.github.com>
2026-05-23 15:09:34 +0300

    Added Diamond pattern
    - added logic in comments step-by-step

--- 02-problem-soving/star-patterns/star_patterns.py
+++ 02-problem-soving/star-patterns/star_patterns.py
@@ -78,6 +78,12 @@ for i in range(rows, 0, -1):
 # ------------------------------------------------------------------------------
 # 04 Pyramid Pattern
 # ------------------------------------------------------------------------------
+""" Instead of writing code first, always think:
+for each row:
+    print spaces
+    print stars
+    move to next line
+"""
 
 """My thoughts,
 
@@ -122,13 +128,6 @@ This combination creates a centered geometric shape (triangle).
 """
 
 
-""" Instead of writing code first, always think:
-for each row:
-    print spaces
-    print stars
-    move to next line
-"""
-
 pattern_title("Pyramid Pattern")
 
 rows = 4
@@ -143,3 +142,162 @@ for i in range(rows):
         print("*", end=" ")
 
     print()
+
+
+# ------------------------------------------------------------------------------
+# 5. Diamond Pattern
+# ------------------------------------------------------------------------------
+""" Instead of writing code first, always think:
+for each row:
+    print spaces
+    print stars
+    move to next line
+"""
+
+pattern_title("Diamond Pattern")
+
+rows = 7
+upper_rows = 7 // 2 + 1
+lower_rows = 7 // 2
+
+# upper part
+for i in range(upper_rows):
+    # spaces
+    for j in range((upper_rows - i) - 1):
+        print(" ", end=" ")
+
+    # stars
+    for k in range(i * 2 + 1):
+        print("*", end=" ")
+
+    print()
+
+
+# for i in range(rows // 2 + 1):
+#     # spaces
+#     for j in range(((rows // 2 + 1) - i) - 1):
+#         print(" ", end=" ")
+
+#     # stars
+#     for k in range(i * 2 + 1):
+#         print("*", end=" ")
+#     print()
+
+
+# lower part
+for i in range(lower_rows):
+
+    # spaces
+    # range = 1 2 3
+    for j in range(i + 1):
+        print(" ", end=" ")
+
+    # stars
+    for k in range((lower_rows - i) * 2 - 1):
+        print("*", end=" ")
+
+    print()
+
+
+"""
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+"""
+
+
+"""
+my logic steps:
+i am starting thinking with stars
+
+--------------------------------------------------------------------------------
+for stars = 1 3 5 7...
+
+simple math for create any odd numbers:
+2x + 1
+
+this is why we use:
+i * 2 + 1 (odd number formula)
+
+
+for stars each i:
+0 * 2 + 1 = 1 star
+1 * 2 + 1 = 3 star
+2 * 2 + 1 = 5 star
+3 * 2 + 1 = 7 star (last line for upper part)
+
+this is range(4)
+
+--------------------------------------------------------------------------------
+(this block will come before the star block)
+for spaces = 3 2 1 0
+
+we gonna use decrease formula:
+x - 1 
+
+(use dots instead of spaces better understanding or dont confuse 
+about print(, end=" ") just change it end="")
+think each end with space like grids.
+
+for spaces(or dots) each i:
+4 - 1 = 3 space
+3 - 1 = 2 space
+2 - 1 = 1 space
+1 - 1 = 0 space (last line for upper part)
+
+so this is range(4, 0, -1):
+"""
+
+# for i in range(rows // 2 + 1):
+#     # spaces
+#     for j in range(((rows // 2 + 1) - i) - 1):
+#         print(" ", end=" ")
+
+#     # stars
+#     for k in range(i * 2 + 1):
+#         print("*", end=" ")
+#     print()
+
+
+# lower part
+"""my logic:
+
+so we already printed upper part. lower part is same logic but reversed order:
+
+im gonna start with spaces this time:
+
+1 - 1 = 0 space (last line for upper part)
+for spaces(or dots) each i:
+2 - 1 = 1 space
+3 - 1 = 2 space
+4 - 1 = 3 space
+
+this is range(1, 4)
+
+
+3 * 2 + 1 = 7 star (remained line from upper part)
+for stars each i:
+2 * 2 + 1 = 5 star
+1 * 2 + 1 = 3 star
+0 * 2 + 1 = 1 star
+
+this is range(2, 0, -1)
+
+"""
+# 3.5 = 0 1 2 = i
+# for i in range(rows // 2):
+
+#     # spaces
+#     # range = 1 2 3
+#     for j in range(i + 1):
+#         print(" ", end=" ")
+
+#     # stars
+#     for k in range((rows // 2 - i) * 2 - 1):
+#         print("*", end=" ")
+
+#     print()
