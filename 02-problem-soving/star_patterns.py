0e5f9059c9dac0a619580bf0644ea14e8eec3521
B <65490597+baus5@users.noreply.github.com>
2026-05-23 15:48:37 +0300

    Added clean comments
    - removed old comments, changed with more cleaner versions
    - files moved to parent folder(problem-solving)

--- 02-problem-soving/star_patterns.py
+++ 02-problem-soving/star_patterns.py
@@ -28,6 +28,15 @@ Examples:
 - range(i, j) produces i, i+1, i+2, ..., j-1.
 """
 
+# Before the start
+"""
+Instead of writing code first, always think:
+for each row:
+    print spaces
+    print stars
+    move to next line
+"""
+
 
 # Terminal Output setting for better visualize
 def pattern_title(title):
@@ -78,32 +87,6 @@ for i in range(rows, 0, -1):
 # ------------------------------------------------------------------------------
 # 04 Pyramid Pattern
 # ------------------------------------------------------------------------------
-""" Instead of writing code first, always think:
-for each row:
-    print spaces
-    print stars
-    move to next line
-"""
-
-"""My thoughts,
-
-At each step we decrease and expand — we decrease spaces and increase stars.
-In the code context, we work with lines. So each line, for example,
-includes an even number of spaces and an odd number of stars.
-Basically, line by line, this creates asymmetry. (Maybe except last line)
-
-But in conclusion, we create a shape, and this shape has symmetry,
-so we are essentially creating symmetry from asymmetry.
-"""
-
-
-"""
-Pyramid patterns:
-step-by-step construction of a symmetric shape using
-shifting offsets and odd-width growth
-"""
-
-
 """
 PYRAMID PATTERN - CORE IDEA
 
@@ -118,15 +101,14 @@ A pyramid is built row by row using two controlled changes:
    - Always grows in an odd pattern (1, 3, 5, 7...)
 
 FORMULAS:
-- spaces = rows - i - 1
-- stars  = 2 * i + 1
+- spaces = rows - i - 1 (x - 1)
+- stars  = 2 * i + 1    (2x + 1)
 
 BIG IDEA:
 Each row = controlled shift (spaces) + controlled growth (stars)
 
 This combination creates a centered geometric shape (triangle).
 """
-
 
 pattern_title("Pyramid Pattern")
 
@@ -140,19 +122,12 @@ for i in range(rows):
     # stars
     for k in range(2 * i + 1):
         print("*", end=" ")
-
     print()
 
 
 # ------------------------------------------------------------------------------
 # 5. Diamond Pattern
 # ------------------------------------------------------------------------------
-""" Instead of writing code first, always think:
-for each row:
-    print spaces
-    print stars
-    move to next line
-"""
 
 pattern_title("Diamond Pattern")
 
@@ -169,7 +144,6 @@ for i in range(upper_rows):
     # stars
     for k in range(i * 2 + 1):
         print("*", end=" ")
-
     print()
 
 
@@ -184,112 +158,4 @@ for i in range(lower_rows):
     # stars
     for k in range((lower_rows - i) * 2 - 1):
         print("*", end=" ")
-
     print()
-
-
-"""
-   *
-  ***
- *****
-*******
- *****
-  ***
-   *
-"""
-
-
-"""
-my logic steps:
-i am starting thinking with stars
-
---------------------------------------------------------------------------------
-for stars = 1 3 5 7...
-
-simple math for create any odd numbers:
-2x + 1
-
-this is why we use:
-i * 2 + 1 (odd number formula)
-
-
-for stars each i:
-0 * 2 + 1 = 1 star
-1 * 2 + 1 = 3 star
-2 * 2 + 1 = 5 star
-3 * 2 + 1 = 7 star (last line for upper part)
-
-this is range(4)
-
---------------------------------------------------------------------------------
-(this block will come before the star block)
-for spaces = 3 2 1 0
-
-we gonna use decrease formula:
-x - 1 
-
-(use dots instead of spaces better understanding or dont confuse 
-about print(, end=" ") just change it end="")
-think each end with space like grids.
-
-for spaces(or dots) each i:
-4 - 1 = 3 space
-3 - 1 = 2 space
-2 - 1 = 1 space
-1 - 1 = 0 space (last line for upper part)
-
-so this is range(4, 0, -1):
-"""
-
-# for i in range(rows // 2 + 1):
-#     # spaces
-#     for j in range(((rows // 2 + 1) - i) - 1):
-#         print(" ", end=" ")
-
-#     # stars
-#     for k in range(i * 2 + 1):
-#         print("*", end=" ")
-#     print()
-
-
-# lower part
-"""my logic:
-
-so we already printed upper part. lower part is same logic but reversed order:
-
-im gonna start with spaces this time:
-
-1 - 1 = 0 space (last line for upper part)
-for spaces(or dots) each i:
-2 - 1 = 1 space
-3 - 1 = 2 space
-4 - 1 = 3 space
-
-this is range(1, 4)
-
-
-3 * 2 + 1 = 7 star (remained line from upper part)
-for stars each i:
-2 * 2 + 1 = 5 star
-1 * 2 + 1 = 3 star
-0 * 2 + 1 = 1 star
-
-this is range(2, 0, -1)
-
-"""
-# 3.5 = 0 1 2 = i
-# for i in range(rows // 2):
-
-#     # spaces
-#     # range = 1 2 3
-#     for j in range(i + 1):
-#         print(" ", end=" ")
-
-#     # stars
-#     for k in range((rows // 2 - i) * 2 - 1):
-#         print("*", end=" ")
-
-#     print()
-
-
-# Re organize comments
